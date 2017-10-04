d427fff720aa6fd9856950dd404086840da77bb9
Eleanor RooseveltDoug <dpjames29@gmail.com>
2017-10-03 21:58:05 -0500

    worked on pig latin method

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,1 +1,12 @@
-# Your code here
+def convert_word_to_pig_latin(word)
+  if word.downcase.start_with?('a','e','i','o','u')
+    word
+  else
+    word = word.split(//)
+      
+    word << word.shift
+    word.join + "AY"
+  end
+end
+
+convert_word_to_pig_latin("SHIT")
